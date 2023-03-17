42bf20f4812654dde32ca2a13dfec6e3635339a5
brian capah <briansabbath@klique.id>
2023-03-17 20:10:23 +0700

    add router and refactor code

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -9,13 +9,13 @@ import common from '../common';
 
 export default function Container({ children }) {
     return <div className='opacity-0 flex min-w-full min-h-screen fade-in'>
-        <div className='min-w-[50vw] h-screen hidden lg:block'>
+        <div className='w-full h-screen hidden lg:block'>
             <div className='min-h-screen flex items-center justify-center min-w-full transition-all duration-300  bg-gradient-to-tr from-seeds-green  to-seeds-purple px-[8%] py-[3%]'>
                 <img className='fixed bottom-0 w-1/2 left-0 z-0' src={bgLine} alt="" />
                 <img className='left-0 w-[30vw]' src={hello} alt="" />
             </div>
         </div>
-        <div className='min-w-[50vw] h-screen relative'>
+        <div className='w-full h-screen relative'>
             <div className='absolute h-full w-full z-20'>
                 <div className='w-full flex items-center justify-center lg:px-20 lg:py-14 h-full p-8 '>
                     <div className='bg-white bg-opacity-50 min-w-full min-h-full py-12 px-8 lg:px-16 relative rounded-2xl shadow-center text-sm lg:text-base flex flex-col justify-between'>
